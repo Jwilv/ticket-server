ec5c2d324262f4955345a379316832e4198b7e49
Jwilv <juan.ignacio.wilvers@gmail.com>
2023-02-07 19:30:28 -0300

    delet de band

--- models/sockets.js
+++ models/sockets.js
@@ -1,4 +1,4 @@
-const BandList = require("./band-list");
+
 
 
 class Sockets {
@@ -6,7 +6,6 @@ class Sockets {
     constructor(io) {
         this.io = io;
 
-        this.bandList = new BandList();
 
         this.socketEvents();
     }
@@ -18,28 +17,7 @@ class Sockets {
 
 
             //Emitir al cliente conectado todas las bandas actuales
-            socket.emit('current-bands', this.bandList.getBands())
-
-            socket.on('votar-band', (id)=>{
-                this.bandList.increaseVotes(id);
-                this.io.emit('current-bands', this.bandList.getBands())
-            })
-
-            socket.on('delete-band', (id)=>{
-                this.bandList.removeBands(id);
-                this.io.emit('current-bands', this.bandList.getBands())
-            })
-
-            socket.on('change-name', ({id,name})=>{
-                this.bandList.changeName(id,name);
-                this.io.emit('current-bands', this.bandList.getBands())
-            })
-
-            socket.on('add-band', ({name})=>{
-                this.bandList.addBand(name);
-                this.io.emit('current-bands', this.bandList.getBands())
-            })
-
+            
         })
 
 
